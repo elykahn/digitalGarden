67d2bd002809213448af46f8209a976d74bada56
Ely Kahn <ely.kahn@gmail.com>
2021-07-20 18:54:04 -0600

    Config Initial
    Initial changes to config nuxt

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
-      title="About Us"
-      subtitle="Example About Page"
+      title="About Me"
+      subtitle="Ely Kahn"
       image="/uploads/about-hero.jpg"
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Fake Heading</h3>
+          <h3>Temp Heading</h3>
           <p>
             <strong>
               Edit this page in<code>/pages/about.vue</code>to fit your needs.
